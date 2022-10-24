39ff979f58c42c5bf15b56a00109bbac5d8f24aa
YuriiSenenko <yurkosenenko@gmail.com>
2022-10-24 20:49:44 +0300

    Task 1 update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
 export const Statistics = ({ good, neutrals, bad }) => {
+  const total = countTotalFeedback({ good, neutrals, bad });
+  const positiveFeedback = countPositiveFeedbackPercentage(total, good);
   return (
     <div>
       <p>Good: {good}</p>
       <p>Neutral: {neutrals}</p>
       <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive feedback: {positiveFeedback}%</p>
     </div>
   );
 };
+
+function countTotalFeedback({ good, neutrals, bad }) {
+  return good + neutrals + bad;
+}
+
+function countPositiveFeedbackPercentage(total, good) {
+  return (total / good).toFixed(1);
+}
